79338cf0efc4e498fea3f1c8799aa5fba82a54bf
tiemimaeda <ptiemi.maeda@gmail.com>
2025-08-12 16:47:48 -0300

    adding the food

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 
@@ -10,6 +11,7 @@ screen.title("My Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -21,8 +23,11 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+
 
 
 screen.exitonclick()
