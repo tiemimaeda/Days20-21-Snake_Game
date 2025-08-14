46f9b64cc6773804a45c2abb7b26fe587e631416
tiemimaeda <ptiemi.maeda@gmail.com>
2025-08-13 23:56:31 -0300

    extending the snake

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Screen
+from scoreboard import Scoreboard
 from snake import Snake
 from food import Food
 import time
@@ -12,6 +13,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -21,13 +23,27 @@ screen.onkey(snake.right, "Right")
 
 game_is_on = True
 while game_is_on:
+    scoreboard.update_scoreboard()
     screen.update()
     time.sleep(0.1)
     snake.move()
 
+    #Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+    
+    #Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
 
-
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -15,11 +15,17 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
+            self.add_segment(position)
+
+    def add_segment(self, position):
             new_segment = Turtle("square")
             new_segment.color("white")
             new_segment.penup()
             new_segment.goto(position)
             self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segments)-1, 0, -1):
@@ -27,7 +33,6 @@ class Snake:
             new_y = self.segments[seg_num -1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
         self.head.forward(MOVE_DISTANCE)
-
 
     def up(self):
         if self.head.heading() != DOWN:
